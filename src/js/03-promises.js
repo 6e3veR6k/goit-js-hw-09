b9d14e0bb7736b98cd821ff8d95269642d2357d7
bezyn4uk <bezyn4uk@gmail.com>
2022-03-29 07:22:31 +0300

    finished pages designs

---
+++ src/js/03-promises.js
@@ -0,0 +1,11 @@
+import '../../node_modules/modern-normalize/modern-normalize.css';
+import '../sass/main.scss';
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
