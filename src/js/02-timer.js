3ca8fa0bbca2cd92cfa32aea085c682c4b17406e
bezyn4uk <bezyn4uk@gmail.com>
2022-03-31 10:51:17 +0300

    add flatpickr

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,23 @@
-import '../../node_modules/modern-normalize/modern-normalize.css';
+import 'modern-normalize/modern-normalize.css';
 import '../sass/main.scss';
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  dateInput: document.getElementById('datetime-picker'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(refs.dateInput, options);
+
+console.log(refs);
